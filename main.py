61c433abf3ea92b8b092600bbc44703460854143
Jean-Chrisrophe Ozanne <jeanchristopheozanne@gmail.com>
2024-07-05 16:29:24 +0200

    all_books_by_categories_netx

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ import csv
 import requests
 from bs4 import BeautifulSoup
 
-
 # requête sur l'url de la page et extraction des données html
 
 url = "https://books.toscrape.com/"
@@ -14,7 +13,7 @@ soup = BeautifulSoup(response.content, "html.parser")
 #---------------PHASE 1 ---------------------
 
 
-   # Extraction de l'url de la page du produit
+# Extraction de l'url de la page du produit
 
 product_catalogue_url = soup.find_all("ol", class_="row")
 
@@ -24,121 +23,99 @@ for links in product_catalogue_url:
 url_catalogue = link[0].get('href')
 product_page_url = url + url_catalogue
 
-
-
-    # Extraction des données de la page du livre choisi, avec BeautifulSoup
+# Extraction des données de la page du livre choisi, avec BeautifulSoup
 
 
 book_response = requests.get(product_page_url)
 book_soup = BeautifulSoup(book_response.content, "html.parser")
 
-    # extraction des informations produit
+# extraction des informations produit
 
 product_information = book_soup.find("table", class_="table table-striped")
 informations = product_information.find_all("tr")
 
-
-
-    # Extraction de l'UPC
+# Extraction de l'UPC
 
 universal_product_code = informations[0].find("td")
 
-
-    # Extraction du titre du livre
+# Extraction du titre du livre
 
 column = book_soup.find("div", class_="col-sm-6 product_main")
 title = column.find("h1")
 
-
-    # Extraction du prix avec taxes
+# Extraction du prix avec taxes
 
 price_including_tax = informations[3].find("td")
 
-
-    #Extraction du prix sans taxes
+#Extraction du prix sans taxes
 
 price_excluding_tax = informations[2].find("td")
 
-
-
-    # Extraction du nombre de livre disponibles
+# Extraction du nombre de livre disponibles
 
 number_available = informations[5].find("td")
 
-
-
-    # Extraction de la catégorie du livre
+# Extraction de la catégorie du livre
 
 cat = book_soup.find("ul", class_="breadcrumb")
 cat_book = cat.find_all("li")
 category = cat_book[2]
 
-
-    # Desciption du produit
+# Desciption du produit
 
 article = book_soup.find("article", class_="product_page")
 articles = article.find_all("p")
 product_description = articles[3]
 
-
-    # Extraction de l'évaluation du livre
+# Extraction de l'évaluation du livre
 
 rating = column.find_all("p")
 review_rating_text = rating[2]["class"][1]
 
-
-    # Extraction de l'url de l'image
+# Extraction de l'url de l'image
 
 section = book_soup.find("div", class_="col-sm-6")
 image_url_text = section.find("img").get("src")
 image_url = url + image_url_text[6:]
 
-
-
 informations = (product_page_url, universal_product_code.text, title.text,
                 price_including_tax.text, price_excluding_tax.text, number_available.text,
                 category.text.strip(), product_description.text, review_rating_text, image_url)
 
+# Création d'un fichier CSV avec les informations obtenues
 
-    # Création d'un fichier CSV avec les informations obtenues
 
 headers = ["product_page_url", "universal_product_code", "title", "price_including_tax",
-           "price_excluding_tax", "number_available","product_description", "category", "review_rating", "image_url"]
+           "price_excluding_tax", "number_available", "product_description", "category", "review_rating", "image_url"]
 with open("book.csv", "w", newline="") as csv_file:
     writer = csv.writer(csv_file, delimiter=",")
     writer.writerow(headers)
     writer.writerow(informations)
 
-
-
 #---------------PHASE 2 ---------------------
 
-    # je choisis la catégorie Mystery : 32 livres sur 2 pages
+# je choisis la catégorie Mystery : 32 livres sur 2 pages
 
 url_mystery = "https://books.toscrape.com/catalogue/category/books/mystery_3/index.html"
 response_mystery = requests.get(url_mystery)
 mystery_soup = BeautifulSoup(response_mystery.content, "html.parser")
 
-
-    # je vise où se situent l'ensemble des livres de la page (mystery_column)
-    # et je selectionne tous les livres (mystery_books)
+# je vise où se situent l'ensemble des livres de la page (mystery_column)
+# et je selectionne tous les livres (mystery_books)
 
 
 mystery_column = mystery_soup.find("ol", class_="row")
 mystery_books = mystery_column.find_all("li", class_="col-xs-6 col-sm-4 col-md-3 col-lg-3")
 
-    # le lien de chaque livre est situé dans les balises h3 :
+# le lien de chaque livre est situé dans les balises h3 :
 
 mystery_books_urls = mystery_column.find_all("h3")
 
-
-    # je boucle sur l'ensemble des url des livres de la catégorie "Mystery" pour obtenir les informations
-    # de chaque livre de la première page comme dans la phase 1
-
+# je boucle sur l'ensemble des url des livres de la catégorie "Mystery" pour obtenir les informations
+# de chaque livre de la première page comme dans la phase 1
 
 descriptions = []
 for mystery_books_url in mystery_books_urls:
-
     links = mystery_books_url.find_all("a")
     link = links[0]
     books_url = url + "catalogue" + link.get("href")[8:]
@@ -163,13 +140,12 @@ for mystery_books_url in mystery_books_urls:
     review_rating = review_rating_text.split()
     section = books_soup.find("div", class_="col-sm-6")
     image_url_text = section.find("img").get("src")
-    image_url = url+image_url_text[6:]
+    image_url = url + image_url_text[6:]
+
     description = [books_url, universal_product_code.text, title.text, price_including_tax.text,
                    price_excluding_tax.text, number_available.text, product_description.text,
-                   category.text.strip(),"".join(review_rating), image_url]
+                   category.text.strip(), "".join(review_rating), image_url]
     descriptions.append(description)
-
-
 
     # cas où la catégorie comporte plusieurs pages, je recommence le processus ci-dessus avec les
     # pages "next" détectées
@@ -178,7 +154,7 @@ next_page = mystery_soup.find("li", class_="next")
 
 while next_page is not None:
     next_page_relative_url = next_page.find("a")["href"]
-    next_page_url = url_mystery[0:len(url_mystery)-10] + next_page_relative_url
+    next_page_url = url_mystery[0:len(url_mystery) - 10] + next_page_relative_url
     page = requests.get(next_page_url)
     soup2 = BeautifulSoup(page.content, "html.parser")
     next_page = soup2.find("li", class_="next")
@@ -215,19 +191,21 @@ while next_page is not None:
         image_url_text = section.find("img").get("src")
         image_url_next = url + image_url_text[6:]
 
-        description_next = [books_next_page_urls, universal_product_code.text, title.text,price_including_tax.text,
+        description_next = [books_next_page_urls, universal_product_code.text, title.text, price_including_tax.text,
                             price_excluding_tax.text, number_available.text, product_description.text,
-                            category.text.strip(),"".join(review_rating), image_url_next]
+                            category.text.strip(), "".join(review_rating), image_url_next]
         descriptions_next.append(description_next)
 
-
-
 # Création d'un fichier CSV avec les informations obtenues
+
+# les descriptions des livres de la première page et des pages suivantes sont placées
+# dans une nouvelle variable
 
 description_next = sum(descriptions_next, [])
 
 full_description = description + description_next
 full_descriptions = descriptions + descriptions_next
+
 
 
 
@@ -240,21 +218,24 @@ with open("full_category.csv", "w", newline="") as csv_file:
     for full_description in full_descriptions:
         writer.writerow(full_description)
 
-
-
-
 #---------------PHASE 3 ---------------------
 
 
-    #extraction des catégories de livres
+#extraction des urls des catégories de livres
 
 aside = soup.find("div", class_="side_categories")
 categories = aside.find("ul").find("li").find("ul")
 urls_categories = categories.find_all("li")
 
+# on boucle sur l'ensemble des premières pages des catégories pour obtenir
+# les url de chaque livre
 
+all_books_by_categories = []
 descriptions_books_categories = []
 for urls_category in urls_categories:
+
+    all_books_by_categories.append([])
+
     links = urls_category.find_all("a")
     link = links[0]
     category_urls = url + link.get("href")
@@ -300,19 +281,29 @@ for urls_category in urls_categories:
                                         product_description.text.strip(),
                                         category.text.strip(), "".join(review_rating), image_url_category_first_page]
 
-        descriptions_books_categories.append(description_books_categories)
+        all_books_by_categories[-1].append(description_books_categories)
 
-        filename = title.text
-        image = requests.get(image_url_category_first_page)
-        with open(filename.strip().replace("#", "").replace("?", "").replace(":", "").replace("*","").replace('"', "") + ".jpg", "wb") as f:
-            f.write(image.content)
 
+
+
+
+
+# téléchargement des images (phase 4) des livres des premières pages de chaque catégorie
+
+# filename = title.text
+# image = requests.get(image_url_category_first_page)
+# with open(filename.strip().replace("#", "").replace("?", "").replace(":", "").replace("*","").replace('"', "") + ".jpg", "wb") as f:
+#     f.write(image.content)
 
 
 # on réitère le processus pour chaque "page suivante" de chaque catégorie
 
+
+all_books_by_categories_next = []
 descriptions_next_category = []
 for urls_category in urls_categories:
+
+    all_books_by_categories_next.append([])
     links = urls_category.find_all("a")
     link = links[0]
     category_urls = url + link.get("href")
@@ -332,8 +323,8 @@ for urls_category in urls_categories:
         category_column_next = soup2.find("ol", class_="row")
         category_books_next = category_column_next.find_all("li", class_="col-xs-6 col-sm-4 col-md-3 col-lg-3")
         category_books_urls_next = category_column_next.find_all("h3")
-
         for category_books_url_next in category_books_urls_next:
+
             links = category_books_url_next.find_all("a")
             link = links[0]
             books_next_page_urls = url + "catalogue" + link.get("href")[8:]
@@ -360,63 +351,44 @@ for urls_category in urls_categories:
             image_url_text = section.find("img").get("src")
             image_url_next = url + image_url_text[6:]
 
-            filename = title.text
-            image = requests.get(image_url_next)
-            with open(filename.strip().replace("#", "").replace("?", "").replace(":", "")
-                              .replace("*", "").replace('"',"").replace("/","") + ".jpg","wb") as f:
-                f.write(image.content)
+            # téléchargement des images (phase 4) des livres des pages suivantes de chaque catégorie
 
-
+            # filename = title.text
+            # image = requests.get(image_url_next)
+            # with open(filename.strip().replace("#", "").replace("?", "").replace(":", "")
+            #                   .replace("*", "").replace('"',"").replace("/","") + ".jpg","wb") as f:
+            #     f.write(image.content)
 
             description_next_category = [books_next_page_urls, universal_product_code.text, title.text,
                                          price_including_tax.text, price_excluding_tax.text, number_available.text,
-                                         product_description.text.strip(), category.text.strip(), "".join(review_rating), image_url_next]
-            descriptions_next_category.append(description_next_category)
+                                         product_description.text.strip(), category.text.strip(),
+                                         "".join(review_rating), image_url_next]
+
+            all_books_by_categories_next[-1].append(description_next_category)
 
 
-
-
-# Création d'un fichier CSV avec les informations obtenues
-
-
-# description_next_category = sum(descriptions_next_category, [])
-# full_description_category = description_books_categories + description_next_category
-# full_descriptions_category = descriptions_books_categories + descriptions_next_category
-#
-# cat = []
-# for category in categories.children:
-#   cat.append(category.text.strip())
-#   categories_list = list(set(cat))
-#   categories = categories_list[1:]
-#
-# for category in categories:
-#
-#   headers = ["books_url", "universal_product_code", "title", "price_including_tax",
-#              "price_excluding_tax", "number_available", "product_description",
-#              "category", "review_rating", "image_url"]
+# description_next_category = sum(description_next_category, [])
+# full_description_categorie = description_books_categories + description_next_category
+# all_books = all_books_by_categories + all_books_by_categories_next
 #
 #
-#   with open(category + ".csv", "w", encoding='utf-8', newline="") as csv_file:
-#       writer = csv.writer(csv_file, delimiter=",")
-#       writer.writerow(headers)
-#       for full_description_category in full_descriptions_category:
-#           writer.writerow(full_description_category)
+#
+# for i in range(50):
+#     categories_name = all_books[i][0][7]
+#
+#
+#     headers = ["books_url", "universal_product_code", "title",
+#                "price_including_tax",
+#                "price_excluding_tax", "number_available", "product_description",
+#                "category", "review_rating", "image_url"]
+#
+#     with open(categories_name + ".csv", "w", encoding='utf-8', newline="") as csv_file:
+#         writer = csv.writer(csv_file, delimiter=",")
+#         writer.writerow(headers)
+#         for full_description_categorie in all_books:
+#             writer.writerow(full_description)
 
 
-
-
-
-
-
-#---------------PHASE 4 ---------------------
-
-
-
-            # filename = title.text
-            # image = requests.get(image_url_category_first_page)
-            # print("ecriture du fichier image")
-            # with open("image.jpg", "wb") as f:
-            #     f.write(image.content)
 
 
 
